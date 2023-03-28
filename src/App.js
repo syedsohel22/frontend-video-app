452f5dbfdf39566f0c247927cc843607c071fb27
sohel <mr.sohelsyed@gmail.com>
2023-03-28 06:28:27 +0530

    project setting-up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import MainRoutes from "./pages/MainRoutes";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div>
+      <MainRoutes />
+    </div>
+  );
 }
 
 export default App;
--- src/pages/MainRoutes.jsx
+++ src/pages/MainRoutes.jsx
@@ -1,7 +1,16 @@
 import React from "react";
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./Home";
+import History from "./History";
+import Bucket from "./Bucket";
 const MainRoutes = () => {
-  return <div>MainRoutes</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/history" element={<History />}></Route>
+      <Route path="/bucket" element={<Bucket />}></Route>
+    </Routes>
+  );
 };
 
 export default MainRoutes;
